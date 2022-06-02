c78ff13fa6b13c4cfae636c37a52ed539105bdaf
1225143 <nagoya.akihiro@eneos.com>
2022-06-02 09:49:28 +0900

    fix: import AN.md

--- AN/md/__Init__.py
+++ AN/md/__Init__.py
@@ -1,3 +1,3 @@
-from .lammps import Lammps
-from .tunedOPLS import refine_opls_via_pfp
-from .ligpargen import LigParGen
+from . lammps import Lammps
+from . tunedOPLS import refine_opls_via_pfp
+from . ligpargen import LigParGen
